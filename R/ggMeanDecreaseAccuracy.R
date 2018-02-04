b22e625d2c036ea82603efd0c03a78fd3e52c32e
EnriquePH <eph.project1500@gmail.com>
2018-02-04 11:05:53 +0100

    ggMeanDecreaseAccuracy

--- R/ggMeanDecreaseAccuracy.R
+++ R/ggMeanDecreaseAccuracy.R
@@ -1,19 +1,22 @@
 #  ----------------------------------------------------------------------------
 #  ggmodel PACKAGE
 #  ggplot2 Machine Learning models
-#  File: ggMeanDecreaseGini.R
+#  File: ggMeanDecreaseAccuracy.R
 #  (c) 2018 - Enrique Pérez Herrero
 #  email: eph.project1500@gmail.com
 #  The MIT License (MIT)
 #  ----------------------------------------------------------------------------
 
-#' ggplot Mean Decrease Gini
+# PLOT RF MeanDecreaseAccuracy
+# Only works if importance = TRUE
+
+#' ggplot Mean Decrease Accuracy
 #'
 #' @param model An object of class \code{randomForest}
 #' @param num An integer with the numbers of features to plot
 #' @param ... Other arguments
 #'
-#' @return A \code{ggplot2} graphic object with feature mean Gini in
+#' @return A \code{ggplot2} graphic object with feature accuracy in
 #'  decrease order, or a warning if model \code{importance} is set
 #'  to \code{FALSE}
 #' @importFrom ggplot2 ggplot
@@ -28,29 +31,31 @@
 #' @examples
 #' \dontrun{
 #' # model is a randomForest model
-#' ggMeanDecreaseGini(model$finalModel)
+#' ggMeanDecreaseAccuracy(model$finalModel)
 #' }
 #' @export
-ggMeanDecreaseGini <- function(model, num, ...) {
-  UseMethod("ggMeanDecreaseGini")
+ggMeanDecreaseAccuracy <- function(model, num, ...) {
+  UseMethod("ggMeanDecreaseAccuracy")
 }
 
-#' @method ggMeanDecreaseGini randomForest
+#' @method ggMeanDecreaseAccuracy randomForest
 #' @export
-ggMeanDecreaseGini.randomForest <- function(model, num = 25, ...) {
+ggMeanDecreaseAccuracy.randomForest <- function(model, num = 25, ...) {
   Variable <- NULL
-  MeanDecreaseGini <- NULL
+  MeanDecreaseAccuracy <- NULL
   if (model$param$importance) {
     df <- as.data.frame(model$importance)
     df <- cbind("Variable" = as.factor(row.names(df)), df)
-    df <- utils::head(df[with(df, order(-MeanDecreaseGini, Variable)),], num)
-    p <- ggplot2::ggplot(data = df, aes(
-      x = stats::reorder(Variable, MeanDecreaseGini),
-      y = MeanDecreaseGini,
-      fill = stats::reorder(Variable, MeanDecreaseGini))) +
+    df <-
+      head(df[with(df, order(-MeanDecreaseAccuracy, Variable)), ], num)
+    p <- ggplot2::ggplot(data = df,
+                aes(
+                  x = stats::reorder(Variable, MeanDecreaseAccuracy),
+                  y = MeanDecreaseAccuracy,
+                  fill = stats::reorder(Variable, MeanDecreaseAccuracy)
+                )) +
       ggplot2::coord_flip() +
       ggplot2::xlab("") +
-      ggplot2::ylab("Mean Decrease Gini") +
       ggplot2::guides(fill = FALSE) +
       ggplot2::geom_bar(stat = "identity", width = .90) +
       ggplot2::theme_minimal()
